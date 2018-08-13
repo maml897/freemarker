c5f7012fa2ca648ae842fc6599100fc6bb659478
maml <maml897@163.com>
2018-08-13 10:19:51 +0800

    ok

--- src/main/java/jigndu/Wenti.java
+++ src/main/java/jigndu/Wenti.java
@@ -20,6 +20,7 @@ public class Wenti
 		double d2 = f;
 		System.out.println(d2);
 
+		// 使用BigDecimal(double val)构造函数时仍会存在精度丢失问题，建议使用BigDecimal(String val)
 		// BigDecimal都建议使用字符串传值，传double也不行
 		DecimalFormat decimalFormat = new DecimalFormat("0.##");
 		decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
@@ -27,6 +28,5 @@ public class Wenti
 		System.out.println(decimalFormat.format(new BigDecimal(162.535d)));// 错误
 		System.out.println(decimalFormat.format(new BigDecimal(String.valueOf(162.535d))));// 正确
 		System.out.println(decimalFormat.format(new BigDecimal(String.valueOf(301353.0499999999883584678173065185546875d))));// 但是如果String.valueOf都不对的话，那结果也不对
-
 	}
 }
